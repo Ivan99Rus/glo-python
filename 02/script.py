8457c639e8bd61abc18920a5f23bd18807437118
Ivan99Rus <ivananapa@mail.ru>
2020-11-01 21:04:41 +0300

    homework 2

---
+++ 02/script.py
@@ -0,0 +1,89 @@
+count = 15
+mark = 5
+sum = 15000
+print(count, type(count))
+print(mark, type(mark))
+print(sum, type(sum))
+
+price = 3.75
+salary = 15.600
+average = 4.86
+print(price, type(price))
+print(salary, type(salary))
+print(average, type(average))
+
+lang = 'python'
+country = 'Russia'
+profession = 'profession'
+print(lang, type(lang))
+print(country, type(country))
+print(profession, type(profession))
+
+arr = [5, 10, 15]
+pupils = ['Ivan', 'Boris', 'Petr']
+courses = ['JavaScript', 'C++', 'Java']
+print(arr, type(arr))
+print(pupils, type(pupils))
+print(courses, type(courses))
+
+user = {
+    'login': 'root',
+    'password': 'qwerty'
+}
+city = {
+    'city': 'Moscow',
+    'population': '12692466'
+}
+resolve = {
+    'edit': True,
+    'add': False
+}
+print(user, type(user))
+print(city, type(city))
+print(resolve, type(resolve))
+
+times = ('14:30', '15:00', '16:30')
+roots = ('edit', 'rewrite', 'add')
+id = (123, 195, 789)
+print(times, type(times))
+print(roots, type(roots))
+print(id, type(id))
+
+answers = {1, 2, 5, 3, 5, 2}
+marks = {2, 5, 3, 3, 4, 5}
+status = {'teacher', 'teacher', 'pupil', 'teacher', 'pupil'}
+print(answers, type(answers))
+print(marks, type(marks))
+print(status, type(status))
+
+is_open = True
+admin = False
+easy = True
+print(is_open, type(is_open))
+print(admin, type(admin))
+print(easy, type(easy))
+
+
+a = 5
+b = 6
+c = 84
+d = 100
+
+amount1 = a + b
+amount2 = d + c
+
+difference1 = a - b
+difference2 = d - a
+
+private1 = d / a
+private2 = c / d
+
+private3 = amount1 // amount2
+private4 = difference2 // difference1
+
+remains1 = private1 % amount2
+remains2 = private4 % private2
+
+composition1 = remains1 * difference1
+composition2 = composition1 * private4
+
